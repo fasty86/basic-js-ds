e85a8af56565a00d6a0cd94f21a05b0260e1b124
fasty86 <oleg.tolm1986@gmail.com>
2023-04-13 11:09:58 +0300

    feat: finish task

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js")
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js")
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(data = null) {
+    this._data = new ListNode(data)
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this._data
+  }
+
+  enqueue(value) {
+    if (this._data.value == null) this._data = new ListNode(value)
+    else {
+      let last = this._data
+      while (last.next) {
+        last = last.next
+      }
+      last.next = new ListNode(value)
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let first = this._data.value
+    this._data = this._data.next
+    return first
   }
 }
+const queue = new Queue()
 
+queue.enqueue(1) // adds the element to the queue
+console.log(queue.getUnderlyingList())
+queue.enqueue(3) // adds the element to the queue
+queue.enqueue(4) // adds the element to the queue
+queue.enqueue(5) // adds the element to the queue
+queue.enqueue(6) // adds the element to the queue
+queue.enqueue(7) // adds the element to the queue
+console.log(queue.getUnderlyingList())
+queue.dequeue() // returns the top element from queue and deletes it, returns 1
+console.log(queue.getUnderlyingList()) // returns { value: 3, next: null }
 module.exports = {
-  Queue
-};
+  Queue,
+}
